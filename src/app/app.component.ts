067ca6a6dc99b733b7c9d2813f7b8d6f2ac71916
CristhianAndres <andres_cri11@hotmail.com>
2024-09-25 15:34:40 -0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
 import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { MatNativeDateModule, DateAdapter, NativeDateAdapter } from '@angular/material/core'; // Importa esto
 import { ReactiveFormsModule } from '@angular/forms'; // Importar ReactiveFormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewsFeedComponent, CommonModule, ReactiveFormsModule],
+  imports: [RouterOutlet, NewsFeedComponent, CommonModule, ReactiveFormsModule, MatNativeDateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
